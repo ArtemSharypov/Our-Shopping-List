09a3969c83e3874f022f43d9c59c75cec9dca40d
ArtemSharypov <artem.sharypov@gmail.com>
2018-01-27 18:15:58 -0600

    Initial Setup
    -Added in Firebase for Authentication / Backend
    -Renamed lists_display fragment
    -Added initial planned fragments and activities as skeletons
    -Split up create item and edit item

---
+++ app/src/main/java/com/artem/ourshoppinglist/SignedinActivity.kt
@@ -0,0 +1,11 @@
+package com.artem.ourshoppinglist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignedinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/artem/ourshoppinglist/SigninFragment.kt
@@ -0,0 +1,14 @@
+package com.artem.ourshoppinglist
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SigninFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
